7be4dd252b6973e46df140cefc9962741fc14b0c
No-sign <55203083+No-sign@users.noreply.github.com>
2020-02-11 17:10:14 +0700

    Add files via upload

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -2,6 +2,21 @@
 #include <string>
 using namespace std;
 
+
+template <class c>
+void sg(c k[],int b){
+	for(int i = 0; i < 10; i++) {
+		if(i==b){
+		cout <<"["<< k[i] <<"]"<< " ";	
+		}
+		else{
+			cout << k[i] << " ";
+		}
+
+}
+}
+
+
 template <typename T>
 void swap(T d[],int x,int y){
     T temp = d[x];
@@ -11,8 +26,30 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
+	int k=1,j=1,num=1,p=0;
+
 	
-//Write your code here.
+	while(k<N)
+	{
+	j=k;
+	sg(d,k);
+	num=k;
+	while(d[j]>d[j-1])
+{
+		swap(d,j,j-1);	
+		num--;
+		if(j>1){	
+		j--;
+	
+		}		
+}
+cout<<"=> ";
+sg(d,num);
+		cout<<"\n";
+		k++;
+	}
+	
+
 
 }
 
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-const int N = 5;
+const int N = 6;
 
 void inputMatrix(double [][N]);
 
@@ -11,13 +11,60 @@ void showMatrix(const bool [][N]);
 
 int main(){
 	double A[N][N]; 
-	bool B[N][N]; 
-	cout << "Input the matrix.....\n";
+	bool B[N][N]={}; 
+	cout << "Input the matrix.....";
+	
 	inputMatrix(A);
 	findLocalMax(A,B);
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
 }
+void inputMatrix(double in[][N]){
+	for(int i=0;i<N;i++){
+	cout<<"\nRow "<<i+1<<": ";
+	for(int j=0;j<N;j++){
+		cin>>in[i][j];	
+	} 
 
-// Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
+	}
+	
+	
+}
+void findLocalMax(const double a[][N], bool b[][N]){
+//	int maxr[5];
+//	int maxc[5];
+//	int k=0;
+	for(int i=0;i<N;i++){
+	for(int j=0;j<N;j++){
+		if(a[i][j]>=a[i+1][j]&&a[i][j]>=a[i][j+1]&&a[i][j]>=a[i-1][j]&&a[i][j]>=a[i][j-1]){
+			b[i][j]=1;
+			
+		}
+
+	}
+	
+}
+	for(int i=0;i<N;i++){
+		b[0][i]=0;
+	}for(int i=0;i<N;i++){
+		b[N-1][i]=0;
+	}
+		for(int i=0;i<N;i++){
+		b[i][0]=0;
+	}
+	for(int i=0;i<N;i++){
+		b[i][N-1]=0;
+	}
+}
+void showMatrix(const bool out[][N])
+{
+	for(int i=0;i<N;i++){
+	for(int j=0;j<N;j++){
+		cout<<out[i][j]<<" ";
+	} 
+		cout << "\n";
+	}
+	
+}
+
